4c949d03aa5fdef77f09e5689dd9fe780770208b
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2022-01-01 22:28:08 +0900

    create serial.h

---
+++ src/serial.h
@@ -0,0 +1,5 @@
+#pragma once
+
+int serial_init(int index);                       /* デバイス初期化 */
+int serial_is_send_enable(int index);             /* 送信可能か？ */
+int serial_send_byte(int index, unsigned char b); /* 1文字送信 */
